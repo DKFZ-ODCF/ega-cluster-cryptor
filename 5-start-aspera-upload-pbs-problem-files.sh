c79fc1616e01d50753864ba7a7547a237acfebe4
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-04-24 15:03:20 +0200

    Delete redundant aspera upload scripts.
    These are basically special cases of the upload, that don't do anything
    differently. In the long run, we want to have a single intelligent
    script that automatically "does the right thing", instead of us poor
    users having to check which of the dozens scripts currently applies.

--- 5-start-aspera-upload-pbs-problem-files.sh
+++
@@ -1,4 +0,0 @@
-bash ega-aspera-get-problem-files.sh > tmp-current-aspera-upload.txt
-cp tmp-current-aspera-upload.txt current-aspera-upload.txt
-rm tmp-current-aspera-upload.txt
-bash start-pbs-aspera-upload.sh
--- 5-start-or-restart-aspera-upload-pbs-all-files.sh
+++
@@ -1,2 +0,0 @@
-ls -1 *.gpg *md5 > current-aspera-upload.txt
-bash start-pbs-aspera-upload.sh
