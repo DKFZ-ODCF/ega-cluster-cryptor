230b7909703c0595cbe2b6be77f9b2884fde8b89
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-04-24 12:51:53 +0200

    adapt 3-check-encryption to use new fileList logic

--- 3-check-encryption-result-all-files.sh
+++ 3-check-encryption-result-all-files.sh
@@ -1,3 +1,13 @@
-echo "in filelist.txt " $(cat fileList.txt | wc -l)
+
+# include utility functions
+source ./util.sh;
+
+OVERRIDE_FILE=$1
+
+# default: use most-recent filelist in current working directory
+FILE_LIST=$(get_default_or_override_fileList $OVERRIDE_FILE);
+verify_fileList $FILE_LIST
+
+echo "in filelist.txt " $(cat $FILE_LIST | wc -l)
 echo "encrypted successfully " $(cat *.pipestatus | grep    "INTERNAL 0 0 0  EXTERNAL 0 0 0  " | wc -l)
 echo "encryption failed "      $(cat *.pipestatus | grep -v "INTERNAL 0 0 0  EXTERNAL 0 0 0  " | wc -l)
