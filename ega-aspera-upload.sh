2dcd829cf3752da8bcec8d9b42ef5651622c6a47
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-05-04 13:02:26 +0200

    ToDo: ega-aspera-upload should take override file-list

--- ega-aspera-upload.sh
+++ ega-aspera-upload.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
 export ASPERA_SCP_PASS="TODO";
 export ASPERA_DESTINATION="ega-box-TODO@fasp.ega.ebi.ac.uk:/."
 export WORKDIR="$PWD";
 cd $WORKDIR
 
+#TODO: make file-list overridable
+# Should take same fileList as script 2 & 3
+# i.e. filelist WITHOUT .gpg and .md5sum in the names
 ls -1 *.gpg *md5 > current-aspera-upload.txt
+
 for FILE in $(cat current-aspera-upload.txt)
 do
   ascp -k2 -Q -l100M -L $WORKDIR $FILE $ASPERA_DESTINATION
