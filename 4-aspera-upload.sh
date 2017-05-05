6214710f1aba62dd452ce3cbe0733291b3dd5681
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-05-05 12:50:37 +0200

    aspera upload: add forgotten uncrypted-checksum, fix ".md5" extension.

--- 4-aspera-upload.sh
+++ 4-aspera-upload.sh
@@ -7,13 +7,20 @@ OVERRIDE_FILE="$1"
 FILE_LIST=$(get_default_or_override_fileList "$OVERRIDE_FILE");
 verify_fileList "$FILE_LIST"
 
+# convert "raw" list of bamfiles to encrypted versions
 UPLOAD_LIST="aspera-upload_$(date '+%Y-%m-%d_%H:%M:%S').txt"
 for FILENAME in $(cat "$FILE_LIST"); do
+  # checksum of unencrypted file
+  if [ -e "$FILENAME.md5" ];
+    echo "$FILENAME.md5" >> "$UPLOAD_LIST"
+  fi
+  # encrypted file
   if [ -e "$FILENAME.gpg" ]; then
     echo "$FILENAME.gpg" >> "$UPLOAD_LIST"
   fi
-  if [ -e "$FILENAME.gpg.md5sum" ];
-    echo "$FILENAME.gpg.md5sum" >> "$UPLOAD_LIST"
+  # checksum of encrypted file
+  if [ -e "$FILENAME.gpg.md5" ];
+    echo "$FILENAME.gpg.md5" >> "$UPLOAD_LIST"
   fi
 done
 
