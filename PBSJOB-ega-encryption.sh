b9cdf243c3824f224949baf3cb2b826e95ca29a5
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2017-11-27 17:10:39 +0100

    encryption: extract re-used filenames into proper variable

--- PBSJOB-ega-encryption.sh
+++ PBSJOB-ega-encryption.sh
@@ -12,6 +12,9 @@ cd $WORKDIR
 
 # extract filename from full path
 FILE=$(basename $FULL_FILE)
+ENCRYPTED_PARTIAL="$FILE.gpg.partial"
+ENCRYPTED_MD5_PARTIAL="$FILE.gpg.md5.partial"
+PLAIN_MD5_PARTIAL="$FILE.md5.partial"
 
 # set temp files
 INTERNAL=$(mktemp --suffix="-encryption-pipestatus-internal")
@@ -24,33 +27,34 @@ TOTAL_PIPESTATUS="$FILE.pipestatus"
 #  and we can calculate the md5 checksums while we have it in memory "anyway".
 cat "$FILE" | tee >(
     gpg -e --always-trust -r EGA_Public_key | tee \
-        "$FILE.gpg.partial" \
-        | md5sum > "$FILE.gpg.md5.partial"; \
+        "$ENCRYPTED_PARTIAL" \
+        | md5sum > "$ENCRYPTED_MD5_PARTIAL"; \
     echo "INTERNAL ${PIPESTATUS[*]}" > "$INTERNAL" \
   ) \
-  | md5sum > $FILE.md5.partial; \
+  | md5sum > "$PLAIN_MD5_PARTIAL"; \
   echo "EXTERNAL ${PIPESTATUS[*]}" > "$EXTERNAL"
 # store combined pipestatus into a file, delete intermediate tempfiles
 echo "$(cat $INTERNAL)  $(cat $EXTERNAL)  $FILE" > "$TOTAL_PIPESTATUS"
 rm "$INTERNAL" "$EXTERNAL"
 
-#replace '-' label (STDIN) in md5 files with the actual file-name used
-sed -i s/-/$FILE.gpg/ "$FILE.gpg.md5.partial"
-sed -i s/-/$FILE/     "$FILE.md5.partial"
+# replace '-' label (STDIN) in md5 files with the actual file-name used
+# to comply with the commonly accepted md5sum fileformat
+sed -i s/-/$FILE.gpg/ "$ENCRYPTED_MD5_PARTIAL"
+sed -i s/-/$FILE/     "$PLAIN_MD5_PARTIAL"
 
 # we're done. Check if everything worked without problems, and
 # rename our .partial files accordingly
 if [ "$(cat $TOTAL_PIPESTATUS)" == "INTERNAL 0 0 0  EXTERNAL 0 0 0  $FILE" ]; then
   # success! no pipes broke :-D
-  mv "$FILE.gpg.partial"     "$FILE.gpg"
-  mv "$FILE.gpg.md5.partial" "$FILE.gpg.md5"
-  mv "$FILE.md5.partial"     "$FILE.md5"
+  mv "$ENCRYPTED_PARTIAL"       "$FILE.gpg"
+  mv "$ENCRYPTED_MD5_PARTIAL"   "$FILE.gpg.md5"
+  mv "$PLAIN_MD5_PARTIAL"       "$FILE.md5"
   rm "$TOTAL_PIPESTATUS" # not needed if everything worked :-)
 else
   # failure! at least one pipe broke :-(
-  mv "$FILE.gpg.partial"     "$FILE.gpg.failed"
-  mv "$FILE.gpg.md5.partial" "$FILE.gpg.md5.failed"
-  mv "$FILE.md5.partial"     "$FILE.md5.failed"
+  mv "$ENCRYPTED_PARTIAL"       "$FILE.gpg.failed"
+  mv "$ENCRYPTED_MD5_PARTIAL"   "$FILE.gpg.md5.failed"
+  mv "$PLAIN_MD5_PARTIAL"       "$FILE.md5.failed"
   # leave $TOTAL_PIPESTATUS around, in case people wish to debug.
   exit 1 # signal non-success to PBS
 fi
