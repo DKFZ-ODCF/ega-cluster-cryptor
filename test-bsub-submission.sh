436eebe71a8121fcac2c2dede5eff13c4e59ca12
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2018-06-29 11:47:38 +0200

    testing script to fiddle with BSUB (not actually tried yet)

---
+++ test-bsub-submission.sh
@@ -0,0 +1,20 @@
+set -eu
+
+MY_DIR="$HOME/lsf-cryptor"
+WORKDIR="$MY_DIR/files"
+JOBLOGDIR="$MY_DIR/cluster-logs"
+SHORTNAME="a.txt"
+FULL_FILE="$WORKDIR/$SHORTNAME"
+#REQ_WALLTIME="00:19:59"
+REQ_WALLTIME="00:09"
+SCRIPT="PBSJOB-ega-encryption.sh"
+SUBMITLOG="$MY_DIR/submit.log"
+
+    bsub \
+        -env "FULL_FILE=$FULL_FILE, WORKDIR=$WORKDIR" \
+        -J "egacrypt-$SHORTNAME" \
+        -Jd "encrypting $SHORTNAME ($FULL_FILE) for the EGA archive" \
+        -e "$JOBLOGDIR/%J.err" \
+        -o "$JOBLOGDIR/%J.out" \
+        -W $REQ_WALLTIME \
+        < "$SCRIPT" | tee -a "$SUBMITLOG"
